6133ee13c067127db35980d7588c8e734deecff3
Caio Oliveira de Araújo <caiooliveira9.co@gmail.com>
2023-09-07 21:01:52 -0300

    Fix

--- src/post/post.controller.js
+++ src/post/post.controller.js
@@ -70,7 +70,7 @@ class PostController {
           message: error.message,
         })
       }
-
+      console.log(error)
       return res.status(500).json({
         message: error.message,
       })
