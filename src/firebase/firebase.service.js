d856e46225e4ff0d24a4dd3083564d510d34cedd
Caio Oliveira de Araújo <caiooliveira9.co@gmail.com>
2023-09-07 21:21:20 -0300

    fix

--- src/firebase/firebase.service.js
+++ src/firebase/firebase.service.js
@@ -10,7 +10,9 @@ class FirebaseService {
       project_id: process.env.FIREBASE_PROJECT_ID,
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
       private_key: process.env.FIREBASE_PRIVATE_KEY
-        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+        ? process.env.FIREBASE_PRIVATE_KEY.split(String.raw`\n`).join(
+            "\n"
+          )
         : undefined,
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
